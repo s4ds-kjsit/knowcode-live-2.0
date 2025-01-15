359383ca3d1b59ffc5308c24a260293ec4a13c18
R <110241863+R-G-KJSIT@users.noreply.github.com>
2025-01-15 10:22:06 +0000

    Env

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,20 @@ import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 
+
+import * as dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBg2STmAblLlasomCu9dhXqsFCmwdQH9zg",
-  authDomain: "knowcode-2-0.firebaseapp.com",
-  projectId: "knowcode-2-0",
-  storageBucket: "knowcode-2-0.firebasestorage.app",
-  messagingSenderId: "426078506744",
-  appId: "1:426078506744:web:2fb7b76d7927741f7eb50d"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
