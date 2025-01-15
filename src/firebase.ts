e742c930e99ed3119aae341e3d21235054cd3923
R <110241863+R-G-KJSIT@users.noreply.github.com>
2025-01-15 10:51:02 +0000

    net

--- src/firebase.ts
+++ src/firebase.ts
@@ -12,12 +12,12 @@ dotenv.config();
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.NETLIFY_FIREBASE_API_KEY,
+  authDomain: process.env.NETLIFY_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NETLIFY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NETLIFY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NETLIFY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NETLIFY_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
